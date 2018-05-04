98ab9b7cbc80055aea8bd22be271fcc223678233
Scott Hall <scotthall127@gmail.com>
2018-05-04 02:30:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board(board = " ")
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}
 -----------
  #{board[3]} | #{board[4]} | #{board[5]}
