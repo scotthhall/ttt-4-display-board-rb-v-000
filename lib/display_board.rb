564bf9523973cc795398188fafd675b2e5809b8c
Scott Hall <scotthall127@gmail.com>
2018-05-04 02:31:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,5 @@ def display_board(board)
 -----------
  #{board[3]} | #{board[4]} | #{board[5]}
 -----------
- #{board[6]} | #{board[7]} | #{board[8]} "
+ #{board[6]} | #{board[7]} | #{board[8]}  "
  end
